3adfe63a9dc2ab2ad8bd8b93e120057a88f50e66
EC2 Default User <ec2-user@ip-172-31-35-242.ec2.internal>
2021-01-07 07:28:44 +0000

    [finish]4-3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+  def show
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+private
+def blog_params
+  params.require(:blog).permit(:title, :category, :body)
+end
 end
